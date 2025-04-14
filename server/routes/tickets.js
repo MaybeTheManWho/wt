1be3c65e625e5fc5c2f5812d908dcbaa5b8fc859
MaybeTheManWho <samaiscool7@gmail.com>
2025-04-14 10:34:02 +0300

    Delete server directory

--- server/routes/tickets.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Define basic routes (you can add actual functionality later)
-router.get('/', (req, res) => {
-  res.json({ message: 'This route is not fully implemented yet' });
-});
-
-module.exports = router;
